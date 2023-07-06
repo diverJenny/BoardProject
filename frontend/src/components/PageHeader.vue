6d088ca2a60a7921981c05bf774d74ff24319af3
jenny <jenny.es2005@gmail.com>
2023-07-06 18:08:53 +0900

    [feat] API 구현중..

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -1,9 +1,10 @@
 <!-- PageHeader.vue -->
 <template>
   <header>
+    <link rel="icon" href="/public/favicon.ico" type="image/x-icon">
     <div id="nav">
-      <router-link to="/">OK Board</router-link>
-      <router-link to="/login">Login</router-link>
+      <router-link to="/">OK Board</router-link> |
+      <router-link to="/login">Login</router-link> |
       <router-link to="/signUp">Sign Up</router-link>
     </div>
   </header>
